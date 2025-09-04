f471a29427511820390579bcf0232b04c077df80
Mufid <mufidafif@icloud.com>
2025-09-04 11:33:45 +0700

    feature: Initial implementation of user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.find_or_create_by(name: 'mufid')
+User.find_or_create_by(name: 'chisato')
+User.find_or_create_by(name: 'takina')
