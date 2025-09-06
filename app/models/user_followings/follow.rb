1b94ec5bb74101990bacf86b42632aab778582fe
Mufid <mufidafif@icloud.com>
2025-09-06 16:54:11 +0700

    feature: Don't allow following self

--- app/models/user_followings/follow.rb
+++ app/models/user_followings/follow.rb
@@ -7,6 +7,10 @@ module UserFollowings
     attribute :user_to_follow
 
     def save
+      if current_user.id == user_to_follow.id
+        errors.add(:base, :can_not_follow_self)
+        return false
+      end
       UserFollowing.create(user_id: current_user.id, user_following_id: user_to_follow.id)
     rescue ActiveRecord::RecordNotUnique
       errors.add(:base, :already_followed)
